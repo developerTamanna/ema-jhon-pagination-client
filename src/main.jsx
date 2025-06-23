b1472a39d718b938e98ec8c624ef9caa8643d4d2
developerTamanna <astamanna57@gmail.com>
2025-06-24 03:05:13 +0600

    next priv button

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Shop from './components/Shop/Shop';
-import Home from './components/Layout/Home';
-import Orders from './components/Orders/Orders';
-import Inventory from './components/Inventory/Inventory';
-import Login from './components/Login/Login';
-import cartProductsLoader from './loaders/cartProductsLoader';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Checkout from './components/Checkout/Checkout';
+import Inventory from './components/Inventory/Inventory';
+import Home from './components/Layout/Home';
+import Login from './components/Login/Login';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
+import './index.css';
+import cartProductsLoader from './loaders/cartProductsLoader';
 
 const router = createBrowserRouter([
   {
@@ -21,31 +17,32 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Shop></Shop>
+        element: <Shop></Shop>,
+        loader: () => fetch('http://localhost:5000/productsCount'),
       },
       {
         path: 'orders',
         element: <Orders></Orders>,
-        loader: cartProductsLoader
+        loader: cartProductsLoader,
       },
       {
         path: 'inventory',
-        element: <Inventory></Inventory>
+        element: <Inventory></Inventory>,
       },
       {
-        path:'checkout',
-        element: <Checkout></Checkout>
+        path: 'checkout',
+        element: <Checkout></Checkout>,
       },
       {
         path: 'login',
-        element: <Login></Login>
-      }
-    ]
-  }
-])
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
